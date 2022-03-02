a338e2719d97a278c88709817c19dedab771d282
추아름 <choo.areum@mintech.kr>
2022-03-02 16:47:31 +0900

    Image, Card, State

--- app/src/main/java/com/example/jetpack_compose_learning/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_learning/MainActivity.kt
@@ -4,48 +4,97 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue;
+import androidx.compose.runtime.setValue;
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_learning.ui.theme.Jetpack_compose_learningTheme
+
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            LazyColumn(
-                modifier = Modifier
-                    .background(Green)
-                    .fillMaxWidth(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ){
-                item {
-                    Text("Header")
-                }
-                    items(50){ index ->
-                        Text("글씨 $index")
-                    }
-                item{
-                    Text("Footer")
-                }
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+            ImageCard(
+                modifier =  Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite,
+            ){ favorite ->
+                isFavorite = favorite
+
             }
         }
     }
 }
 
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit,
+    ){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp,
+    ){
+        Box(
+            modifier = Modifier.height(200.dp)
+        ){
+            Image(
+                    painter = painterResource(id = R.drawable.sunny),
+                    contentDescription = "sunny",
+                    contentScale = ContentScale.Crop,
+
+                )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ){
+                IconButton(onClick = {
+                    onTabFavorite(!isFavorite)
+
+                }) {
+                    Icon(
+                        imageVector = if(isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = White,
+                    )
+
+                }
+            }
+
+        }
+    }
+}
