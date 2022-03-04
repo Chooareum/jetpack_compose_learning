79ebf967e7e9656bbf8d28f6be2dfbbe4945aabf
추아름 <choo.areum@mintech.kr>
2022-03-04 15:58:11 +0900

    Xylophone

--- app/src/main/java/com/example/jetpack_compose_learning/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_learning/MainActivity.kt
@@ -1,132 +1,116 @@
 package com.example.jetpack_compose_learning
 
 import android.app.Application
-import android.content.Context
 import android.content.pm.ActivityInfo
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
+import android.media.SoundPool
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
-    private val viewModel by viewModels<MainViewModel>()
+
+    private  val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //화면이 꺼지지 않게 하기
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        //화면이 가로 모드로 고정되게 하기
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
 
-        lifecycle.addObserver(viewModel)
+        setContent{
+            XylophoneScreen(viewModel = viewModel)
+        }
 
-        setContent {
-            TiltScreen(x = viewModel.x.value, y = viewModel.y.value)
+    }
+}
 
+class  MainViewModel(application: Application) : AndroidViewModel(application){
+    private val soundPool = SoundPool.Builder().setMaxStreams(8).build()
+    private val sounds = listOf(
+        soundPool.load(application.applicationContext, R.raw.do1, 1),
+        soundPool.load(application.applicationContext, R.raw.re, 1),
+        soundPool.load(application.applicationContext, R.raw.mi, 1),
+        soundPool.load(application.applicationContext, R.raw.fa, 1),
+        soundPool.load(application.applicationContext, R.raw.sol, 1),
+        soundPool.load(application.applicationContext, R.raw.la, 1),
+        soundPool.load(application.applicationContext, R.raw.si, 1),
+        soundPool.load(application.applicationContext, R.raw.do2, 1),
+
+    )
+    fun playSound(index: Int){
+        soundPool.play(sounds[index], 1f, 1f, 0, 0, 1f)
+    }
+
+    override fun onCleared() {
+        soundPool.release()
+        super.onCleared()
+    }
+
+}
+
+@Composable
+fun XylophoneScreen(
+    viewModel: MainViewModel
+){
+    val keys = listOf(
+        Pair("도", Color.Red),
+        Pair("레", Color(0xFFF9800)),
+        Pair("미", Color(0xFFFC107)),
+        Pair("파", Color(0xFF8BC34A)),
+        Pair("솔", Color(0xFF2196F3)),
+        Pair("라", Color(0xFF3F51B5)),
+        Pair("시", Color(0xFF673AB7)),
+        Pair("도", Color.Red),
+    )
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ){
+        keys.forEachIndexed { index, key ->
+            val padding = (index +2) * 8 // 16, 24, 32...
+            KeyBoard(modifier = Modifier
+                .padding(top = padding.dp, bottom = padding.dp)
+                .clickable {
+                    viewModel.playSound(index)
+                },
+                text = key.first,
+                color = key.second )
         }
     }
 }
 
-class MainViewModel(application: Application) : AndroidViewModel(application), SensorEventListener,
-    LifecycleObserver {
-
-    private val _x = mutableStateOf(0f)
-    val x : State<Float> = _x
-
-    private val _y = mutableStateOf(0f)
-    val y : State<Float> = _y
-
-    private val sensorManager by lazy{
-        application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun registerSensor(){
-        sensorManager.registerListener(
-            this,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_NORMAL,
-        )
-
-
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    fun unregisterSensor(){
-        sensorManager.unregisterListener(this)
-    }
-
-
-    override fun onSensorChanged(event: SensorEvent?) {
-       event?.let{
-           _x.value = event.values[0]
-           _y.value = event.values[1]
-       }
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-
-    }
-}
 
 @Composable
-fun TiltScreen(x: Float, y: Float){
-
-    val yCoord = x * 20
-    val xCoord = y * 20
-
-    Canvas(modifier = Modifier.fillMaxSize()){
-        val centerX = size.width/2
-        val centerY = size.height/2
-
-        //바깥 원
-        drawCircle(
-            color = androidx.compose.ui.graphics.Color.Black,
-            radius = 100f,
-            center = Offset(centerX, centerY),
-            style = androidx.compose.ui.graphics.drawscope.Stroke(),
+fun KeyBoard(
+    modifier: Modifier,
+    text: String,
+    color: Color,
+){
+    Box(
+        modifier = modifier
+            .width(50.dp)
+            .fillMaxHeight()
+            .background(color = color)
+    ){
+        Text(
+            text = text,
+            style = TextStyle(color = Color.White, fontSize = 20.sp),
+            modifier = Modifier.align(Alignment.Center)
         )
-
-        //초록 원
-        drawCircle(
-            color = androidx.compose.ui.graphics.Color.Green,
-            radius = 100f,
-            center = Offset(xCoord + centerX, yCoord+ centerY),
-
-        )
-
-        //가운데 십자가
-        drawLine(
-            color = Color.Black,
-            start = Offset(centerX - 20, centerY),
-            end = Offset(centerX + 20 , centerY),
-        )
-
-        drawLine(
-            color = Color.Black,
-            start = Offset(centerX, centerY - 20),
-            end = Offset(centerX , centerY + 20 ),
-        )
-
-
     }
 }
